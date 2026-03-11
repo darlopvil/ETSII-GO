b8c4da6a829080974f03e8750acd3a43e2a7caa4
darlopvil <darlopvil@alum.us.es>
2026-03-11 10:54:20 +0100

    Configuración de botones en MainActivity y creación del resto de Activities

--- app/src/main/java/es/us/etsii_go/MainActivity.java
+++ app/src/main/java/es/us/etsii_go/MainActivity.java
@@ -1,5 +1,6 @@
 package es.us.etsii_go;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -19,6 +20,37 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Configuración de los 4 botones
+        configurarBotones();
+
+    }
+
+    private void configurarBotones() {
+
+        // 1.- Botón "GPS Facultad"
+        findViewById(R.id.boton_gps_facultad).setOnClickListener(v -> {
+            Intent intent_gps = new Intent(this, RecorridoActivity.class);
+            startActivity(intent_gps);
+        });
+
+        // 2.- Botón "Servicios"
+        findViewById(R.id.boton_servicios).setOnClickListener(v -> {
+            Intent intent_servicios = new Intent(this, ServiciosActivity.class);
+            startActivity(intent_servicios);
+        });
+
+        // 3.- Botón "Itinerario"
+        findViewById(R.id.boton_itinerario).setOnClickListener(v -> {
+            Intent intent_itinerario = new Intent(this, ItinerarioActivity.class);
+            startActivity(intent_itinerario);
+        });
+
+        // 4.- Botón "Información"
+        findViewById(R.id.boton_info).setOnClickListener(v -> {
+            Intent intent_info = new Intent(this, InformacionActivity.class);
+            startActivity(intent_info);
         });
     }
 }
